036fd14ba85fee5425d13ab2f1f5c941eaf794e7
facelessagony <eric.solomon.ca@gmail.com>
2022-11-16 23:01:01 +0000

    add join queries

--- 2_queries_joins/2.sql
+++ 2_queries_joins/2.sql
@@ -1,3 +1,4 @@
-SELECT SUM(assignment_submissions.duration) AS total_duration FROM assignment_submissions JOIN students ON assignment_submissions.student_id = students.id 
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'FEB12';
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name;
